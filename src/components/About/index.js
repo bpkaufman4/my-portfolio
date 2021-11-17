b56904d2ccb057d32573b3f7979823c28a92aafb
Brian Kaufman <bpkaufman4@gmail.com>
2021-11-17 10:17:09 -0600

    added and rendered about section

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    return (
+        <section className="my-5">
+            <h1>About me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate mi sit amet mauris commodo quis imperdiet. Lacinia at quis risus sed vulputate odio ut enim. Mattis nunc sed blandit libero volutpat sed cras. Nulla posuere sollicitudin aliquam ultrices sagittis orci. Integer eget aliquet nibh praesent tristique magna sit amet purus. Elit at imperdiet dui accumsan sit amet. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Rhoncus urna neque viverra justo nec. Accumsan lacus vel facilisis volutpat est velit egestas dui. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Arcu cursus euismod quis viverra nibh cras pulvinar.</p>
+        </section>
+    );
+}
+
+export default About;
