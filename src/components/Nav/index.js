96e304266bdbe30c52346991e0a548c6b3ef48bd
Brian Kaufman <bpkaufman4@gmail.com>
2021-11-17 18:09:22 -0600

    portfolio is rendering

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 
 function Nav(props) {
@@ -8,52 +9,29 @@ function Nav(props) {
         setResumeSelected
     } = props;
 
-    var aboutEl = document.getElementById('about');
-    var portfolioEl = document.getElementById('portfolio');
-    var contactEl = document.getElementById('contact');
-    var resumeEl = document.getElementById('resume');
-
-    var activate = function(activeLink) {
-        activeLink.classList.add('active');
-    };
-
-    var inactive = function(link1, link2, link3) {
-        link1.classList.remove('active');
-        link2.classList.remove('active');
-        link3.classList.remove('active');
-    }
-
     const clickContact = function() {
         setContactSelected(true);
         setResumeSelected(false);
         setPortfolioSelected(false);
         setAboutSelected(false);
-        activate(contactEl);
-        inactive(resumeEl, portfolioEl, aboutEl);
       }
       const clickResume = function() {
         setContactSelected(false);
         setResumeSelected(true);
         setPortfolioSelected(false);
         setAboutSelected(false);
-        activate(resumeEl);
-        inactive(contactEl, portfolioEl, aboutEl);
       }
       const clickPortfolio = function() {
         setContactSelected(false);
         setResumeSelected(false);
         setPortfolioSelected(true);
         setAboutSelected(false);
-        activate(portfolioEl);
-        inactive(resumeEl, contactEl, aboutEl);
       }
       const clickAbout = function() {
         setContactSelected(false);
         setResumeSelected(false);
         setPortfolioSelected(false);
         setAboutSelected(true);
-        activate(aboutEl);
-        inactive(resumeEl, portfolioEl, contactEl);
       }
     
 
@@ -63,12 +41,10 @@ function Nav(props) {
                 <a href="/">Brian Kaufman</a>
             </h2>
             <nav>
-                <ul>
-                    <li><span class={'link active'} id={"about"} onClick={clickAbout}>About Me</span></li>
-                    <li><span class={'link'} id={"portfolio"} onClick={clickPortfolio}>My Portfolio</span></li>
-                    <li><span class={'link'} id={"resume"} onClick={clickResume}>My Resume</span></li>
-                    <li><span class={'link'} id={"contact"} onClick={clickContact}>Contact Me</span></li>
-                </ul>
+                <div key="about" classlist={'link'} id={"about"} onClick={clickAbout}>About Me</div>
+                <div key="portfolio" classlist={'link'} id={"portfolio"} onClick={clickPortfolio}>My Portfolio</div>
+                <div key="resume" classlist={'link'} id={"resume"} onClick={clickResume}>My Resume</div>
+                <div key="contact" classlist={'link'} id={"contact"} onClick={clickContact}>Contact Me</div>
             </nav>
         </header>
     );
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 
 function Portfolio() {
+
+    const projects = [
+        {name:"Coding Quiz",url:"https://bpkaufman4.github.io/coding-quiz/", image:"coding-quiz.png"},
+        {name:"Weather Dashboard", url:"https://bpkaufman4.github.io/weather-dashboard/", image:"weather-dashboard.png"},
+        {name:"Password Generator", url:"https://bpkaufman4.github.io/password-generator/", image:"password-generator.png"},
+        {name:"Old Profile", url:"https://bpkaufman4.github.io/challenge-2/", image:"old-profile.png"}
+    ]
+
     return (
         <section className="my-5">
-            <h1>My portfolio</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate mi sit amet mauris commodo quis imperdiet. Lacinia at quis risus sed vulputate odio ut enim. Mattis nunc sed blandit libero volutpat sed cras. Nulla posuere sollicitudin aliquam ultrices sagittis orci. Integer eget aliquet nibh praesent tristique magna sit amet purus. Elit at imperdiet dui accumsan sit amet. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Rhoncus urna neque viverra justo nec. Accumsan lacus vel facilisis volutpat est velit egestas dui. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Arcu cursus euismod quis viverra nibh cras pulvinar.</p>
+            {projects.map((project) => (
+                <>
+                    <div>
+                        <h2><a href={project.url}>{project.name}</a></h2>
+                    </div>
+                    <a href={project.url}><img alt="screen shot of the project" src={project.image}/></a>
+                </>
+            ))}
         </section>
     );
 }
 
 export default Portfolio;
+
+
+
+// https://bpkaufman4.github.io/coding-quiz/
+// 
+// 
+// https://bpkaufman4.github.io/challenge-2/
